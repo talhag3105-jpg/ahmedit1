feb6a8b02bd2c8f29ec7593b331160d41e5647bf
talhag3105-jpg <talhag3105@gmail.com>
2025-10-17 18:06:36 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,18 @@
+// Firebase yapılandırması
+const firebaseConfig = {
+  apiKey: "AIzaSyC86wSTkGJvDBx7h_NWaY_Iy_4th1_4zRQ",
+  authDomain: "ahmedit1.firebaseapp.com",
+  projectId: "ahmedit1",
+  storageBucket: "ahmedit1.firebasestorage.app",
+  messagingSenderId: "508853189575",
+  appId: "1:508853189575:web:e150c1989cb432754437e2",
+  measurementId: "G-7JH08PKY39"
+};
+
+// Firebase başlat
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 // Firebase bağlantısı
 const firebaseConfig = {
   apiKey: "AIzaSyC86wSTkGJvDBx7h_NWaY_Iy_4th1_4zRQ",
